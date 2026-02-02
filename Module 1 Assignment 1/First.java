faa6bbe24138e3089462f715a7e0ec48d8babcfe
JamuDuck <122187056+JamuDuck@users.noreply.github.com>
2026-02-01 22:27:14 -0500

    drops

---
+++ Module 1 Assignment 1/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args)  {
+    System.out.println("Hola mundo");
+    System.out.println("Job title: Information Security Analysts");
+    System.out.println("Something I'm interested in and good pay");
+    System.out.println("Median salary: $124,910");
+    }
+}
---
+++ Module 2 Assignment 1/Tests.java
@@ -0,0 +1,15 @@
+public class Tests {
+    public static void main(String[] args) {
+        // Test Score Vars //
+        float score1 = 88.2f;
+        float score2 = 78.9f;
+        float score3 = 97.6f;
+
+        // Print Statments //
+        System.out.println("Test score 1: " + score1);
+        System.out.println("Test score 2: " + score2);
+        System.out.println("Test score 3: " + score3);
+        System.out.println("The average of 3 test score is: " + ((score1 + score2 + score3) / 3));
+
+    }
+}
