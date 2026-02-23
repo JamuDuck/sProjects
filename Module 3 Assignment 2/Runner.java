aa31442969629494388842813d7ba6a8e2d6184f
JamuDuck <122187056+JamuDuck@users.noreply.github.com>
2026-02-23 17:28:07 -0500

    test

---
+++ Module 3 Assignment 2/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+
+    public static void main(String[] args){
+        Tests t = new Tests();
+        t.getAverage();
+        System.out.println(t.toString());
+    }
+}
---
+++ Module 3 Assignment 2/Tests.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Tests {
+
+    private double ave;
+    private int count;
+    private int score;
+
+    public Tests() {
+
+    }
+
+    // Getters
+    public double getAve() {
+        return ave;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    // Setter
+    public void setScore(int newScore){
+        score = newScore;
+    }
+
+    public void getAverage(){
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        count = 0;
+
+        System.out.print("Enter a test score (-1 to quit): ");
+        score = input.nextInt();
+
+        while (score != -1) {
+            sum = sum +score;
+            count = count + 1;
+            System.out.print("Enter a test score (-1 to quit): ");
+            score = input.nextInt();
+        }
+        ave = (double) sum / count;
+    }
+
+
+    // Tostring
+    public String toString(){
+        return "The average of the " + count +
+                " scores entered is " +
+                String.format("%.2f", ave) + ".";
+
+    }
+}
